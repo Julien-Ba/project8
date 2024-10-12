1ad77167579e66b2eaf30bd00e10b8ce6baca098
julien-ba <julienbarbier42@gmail.com>
2024-10-12 11:46:43 +0200

    exercice 3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,6 +1,12 @@
-// Créez votre fonction ici
+function calculateAverage(numbers) {
+    const length = numbers?.length;
+    if (!length)
+        return 'No numbers to calculate average';
+    let total = 0;
+    numbers.forEach(number => total += number);
+    return total / length;
+}
 
-// Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
 console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
 console.log(calculateAverage()) // No numbers to calculate average
